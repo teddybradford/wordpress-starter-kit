ee3139e422cc51c949316de2d3e3113d206b215d
Teddy Bradford <3684553+teddybradford@users.noreply.github.com>
2015-11-06 12:40:06 -0500

    Implement install_plugin function

--- install.sh
+++ install.sh
@@ -9,33 +9,28 @@ source "$DIR/config.sh"
 if [ ! -d "wordpress" ]; then
   curl -o wordpress.zip https://wordpress.org/latest.zip
   unzip -q -o wordpress.zip
+  mv wordpress "$WP_DIR"
   rm wordpress.zip
 fi
 
-# Install SQLite Integration plugin
-if [ ! -d "$WP_DIR/wp-content/plugins/sqlite-integration" ]; then
-  curl -o sqlite-integration.zip https://downloads.wordpress.org/plugin/sqlite-integration.zip
-  unzip -q -o -d "$WP_DIR/wp-content/plugins" sqlite-integration.zip
-  rm sqlite-integration.zip
-  cp "$WP_DIR/wp-content/plugins/sqlite-integration/db.php" "$WP_DIR/wp-content"
-  cp "$WP_DIR/wp-config-sample.php" "$WP_DIR/wp-config.php"
-fi
-
-# Install Timber plugin
-if [ ! -d "$WP_DIR/wp-content/plugins/timber-library" ]; then
-  curl -o timber-library.zip https://downloads.wordpress.org/plugin/timber-library.zip
-  unzip -q -o -d "$WP_DIR/wp-content/plugins" timber-library.zip
-  rm timber-library.zip
-fi
-
-# Install ACF PRO plugin
-if [ -n $ACF_KEY ]; then
-  if [ ! -d "$WP_DIR/wp-content/plugins/advanced-custom-fields-pro" ]; then
-    curl -o advanced-custom-fields-pro.zip "http://connect.advancedcustomfields.com/index.php?p=pro&a=download&k=$ACF_KEY"
-    unzip -q -o -d "$WP_DIR/wp-content/plugins" advanced-custom-fields-pro.zip
-    rm advanced-custom-fields-pro.zip
+# Install plugins
+function install_plugin {
+  if [ ! -d "$WP_DIR/wp-content/plugins/$1" ]; then
+    curl -o "$1.zip" $2
+    unzip -q -o -d "$WP_DIR/wp-content/plugins" "$1.zip"
+    rm "$1.zip"
   fi
+}
+install_plugin sqlite-integration https://downloads.wordpress.org/plugin/sqlite-integration.zip
+install_plugin timber-library https://downloads.wordpress.org/plugin/timber-library.zip
+install_plugin post-types-order https://downloads.wordpress.org/plugin/post-types-order.zip
+if [ -n $ACF_KEY ]; then
+  install_plugin advanced-custom-fields-pro "http://connect.advancedcustomfields.com/index.php?p=pro&a=download&k=$ACF_KEY"
 fi
+
+# Configure SQLite Integration
+cp "$WP_DIR/wp-content/plugins/sqlite-integration/db.php" "$WP_DIR/wp-content"
+cp "$WP_DIR/wp-config-sample.php" "$WP_DIR/wp-config.php"
 
 # Install Gulp
 if ! hash gulp 2>/dev/null; then
